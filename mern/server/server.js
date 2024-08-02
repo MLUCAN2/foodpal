06c5620fc896622466a6a2f005c6246f0a29a796
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-08-02 16:34:31 -0500

    updated server.js to include mongoose

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 
 const PORT= process.env.PORT || 3001;
@@ -7,6 +8,12 @@ const app= express();
 app.use(cors());
 app.use(express.json());
 
+// Connection for MongoDB, our DB
+mongoose.connect('mongodb://localhost/mern-stack-db', {
+    useNewUrlParser: true, useUnifiedTopology: True
+});
+
+// Listen for the routes and middleware
 app.listen(PORT, ()=> {
     console.log(`Server is alive on port ${PORT}`)
 })
