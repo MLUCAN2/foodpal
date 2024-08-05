09f7bdb06f9130d9bbf1da88dd0b10f0d6c93f2f
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-08-05 14:26:52 -0500

    updated server folder w/models

--- mern/server/server.js
+++ mern/server/server.js
@@ -2,15 +2,22 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
+// require('dotenv').config();
+
 const PORT= process.env.PORT || 3001;
 const app= express();
 
+
 app.use(cors());
+//Allow us to parse JSON data
 app.use(express.json());
 
 // Connection for MongoDB, our DB
-mongoose.connect('mongodb://localhost/mern-stack-db', {
-    useNewUrlParser: true, useUnifiedTopology: True
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("Connected to MongoDB");
 });
 
 // Listen for the routes and middleware
